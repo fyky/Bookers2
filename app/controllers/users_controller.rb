4f699dd5d361e9b96982a5d614f51be0f2808054
fyky <fuyu12dmb25ms@gmail.com>
2021-10-21 16:25:36 +0000

    [Add]booksページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
   def show
+    @books = Book.all
 
   end
   
   def edit
     @profile_image = User.new
+    @user = User.find(params[:id])
   end
+  
 
   def index
+    @users = User.all
   end
 
 end
