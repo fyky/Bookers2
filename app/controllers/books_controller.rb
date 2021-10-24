bf6a83fb92f0b73ac3dcaebd1adc4f6740ce4ec8
fyky <fuyu12dmb25ms@gmail.com>
2021-10-24 06:55:54 +0000

    [Add]バリデーションメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
     @user = current_user
-    
+
 
   end
 
@@ -18,25 +18,24 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    
+    @user = current_user
+    @books = Book.all
     if @book.save
       flash[:notice]="You have creatad book successfully."
       redirect_to book_path(@book)
     else
-      @user = current_user
-      @books = Book.all
-      render :index
+      render "index"
     end
-  
+
   end
-  
+
   def update
     @books = Book.all
     @book = Book.find(params[:id])
     if @book.update(book_params)
     redirect_to book_path, notice: 'Book was successfully created.'
     else
-    redirect_to book_path
+    render "edit"
     end
 
   end
@@ -49,6 +48,13 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     @user = current_user
+
+
+    if @book.user == current_user
+      render "edit"
+    else
+     redirect_to books_path
+    end
   end
 
 
